faed3aba1eb94a2d5cec28af9eb93525c03196f2
Umar <umar.dev500@gmail.com>
2025-03-28 17:11:47 +0700

    setup

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {withNativeWind} = require('nativewind/metro');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = mergeConfig(getDefaultConfig(__dirname), {
+  /* your config */
+});
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const nativeWindConfig = withNativeWind(config, {input: './src/global.css'});
+module.exports = wrapWithReanimatedMetroConfig(nativeWindConfig);
